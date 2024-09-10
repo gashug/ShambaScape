a2a9f91d65275147f5c52883d91875280dd26dc3
gashug <geoffrey.gachuiri98@gmail.com>
2024-09-10 14:13:55 +0300

    removed User association for design model

--- design/models.py
+++ design/models.py
@@ -1,12 +1,14 @@
 from django.db import models # Importing Django's model library
-from django.contrib.auth.models import User # Importing the User model to associate designs with users
+
 
 # Model representing a garden design
 class Design(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE) # Each design belongs to a user
     name = models.CharField(max_length=100) # Name of the design
     layout_data = models.JSONField() # Stores the design layout as JSON (e.g., positions of plants)
     created_at = models.DateTimeField(auto_now_add=True) # Timestamp when the design was created
+
+    def __str__(self):
+        return self.name # Returns the name of the design when referenced
 
 # Model representing individual elements (like plants) within a design
 class LayoutElement(models.Model):
