146ec4fbc6ce7d46694f869c81d29566c30d06ea
gashug <geoffrey.gachuiri98@gmail.com>
2024-09-11 20:37:55 +0300

    fixed layout_data field for Design model

--- design/models.py
+++ design/models.py
@@ -5,7 +5,7 @@ from django.db import models # Importing Django's model library
 class Design(models.Model):
     name = models.CharField(max_length=100) # Name of the design
     description = models.TextField(blank=True)  # Optional description of the design
-    layout_data = models.JSONField() # Stores the design layout as JSON (e.g., positions of plants)
+    layout_data = models.JSONField(default=dict) # Stores the design layout as JSON (e.g., positions of plants)
     created_at = models.DateTimeField(auto_now_add=True) # Timestamp when the design was created
 
     def __str__(self):
